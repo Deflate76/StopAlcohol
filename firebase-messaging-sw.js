3bcbe873cba7f5bd13be9d84d95e98093953daa4
JHKim <powerzenith@gmail.com>
2026-06-08 16:41:00 +0900

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,35 @@
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "기존 alcoholway apiKey",
+  authDomain: "alcoholway.firebaseapp.com",
+  projectId: "alcoholway",
+  storageBucket: "alcoholway.appspot.com",
+  messagingSenderId: "기존 senderId",
+  appId: "기존 appId"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("백그라운드 푸시 수신:", payload);
+
+  const title = payload.notification?.title || "알림";
+  const options = {
+    body: payload.notification?.body || "",
+    icon: "/icon.png",
+    data: {
+      url: payload.fcmOptions?.link || "/"
+    }
+  };
+
+  self.registration.showNotification(title, options);
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  const url = event.notification.data?.url || "/";
+  event.waitUntil(clients.openWindow(url));
+});
